6c427fd40c9de63653358804c43489e79b463d83
moritamori <tamo@sonicgarden.jp>
2019-05-26 20:28:53 +0900

    routesにrootを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action do
+    if Rails.env.development? || params[:rmp].present?
+      Rack::MiniProfiler.authorize_request
+    end
+  end
 end
